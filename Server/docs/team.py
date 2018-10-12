17ec806a7a25ec545a7a4848046f4f2b1babce46
RISMME <python@istruly.sexy>
2018-10-12 10:24:00 +0900

    team post

--- Server/docs/team.py
+++ Server/docs/team.py
@@ -1,9 +1,10 @@
-from . import parameter
+from . import parameter, jwt_header
 
 TEAM_GET = {
     'tags': ['Team'],
     'description': '팀별 팀원 리스트',
     'parameters': [
+        jwt_header,
         parameter('team', '팀 종류', 'query string')
     ],
     'responses': {
@@ -11,23 +12,40 @@ TEAM_GET = {
             'description': 'get 성공',
             'examples': {
                 '': {
-                    {
-                        'blue': [
+                    [
+                        [
                             'ㅁㅁ',
                             'ㅇㅇ',
                             'ㄷㄷ'
                         ],
-                        'green': [
+                        [
                             'aa',
                             'dd'
                         ],
-                        'yellow': None,
-                        'violet': [
+                        None,
+                        [
                             '상미니'
                         ]
-                    }
+                    ]
                 }
             }
+        }
+    }
+}
+
+TEAM_POST = {
+    'tags': ['Team'],
+    'description': '팀 참가',
+    'parameters': [
+        jwt_header,
+        parameter('team', '팀 종류\n0: blue\n1: green\n2: yellow\n3: violet', 'query string')
+    ],
+    'responses': {
+        '201': {
+            'description': '참가 성공'
+        },
+        '205': {
+            'description': '팀원 초과'
         }
     }
 }
