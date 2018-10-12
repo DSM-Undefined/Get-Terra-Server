e154142a2560d906633813e2a05abfcff4e87e6e
by09115 <by09115@outlook.kr>
2018-10-12 22:45:54 +0900

    [UPDATE] api중 account(auth, signup) 개발 완료

--- Server/view/account/auth.py
+++ Server/view/account/auth.py
@@ -1,11 +1,33 @@
 from flasgger import swag_from
+from flask_restful import Resource, reqparse
+from flask_jwt_extended import create_access_token, create_refresh_token
 
 from view.base_resource import BaseResource
 from docs.account import AUTH_POST
+from model import UserInfo
 
 
 class Auth(BaseResource):
 
     @swag_from(AUTH_POST)
     def post(self):
-        pass
+        parser = reqparse.RequestParser()
+        parser.add_argument('id', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        id_ = parser.parse_args()['id']
+        password_ = parser.parse_args()['password']
+
+        id_in_db = UserInfo.objects(userId=id_)
+        correct_password = UserInfo.object(password=password_)
+        success_201 = {
+            "accessToken": create_access_token(id_),
+            "refreshToken": create_refresh_token(id_)
+        }
+
+        if id_in_db and correct_password:
+            return success_201, 201
+        # 로그인 성공시 액세스 토큰 및 리프레시 토큰을 반환
+
+        else:
+            return 401
+        # 로그인 실패시 status code 401 반환
--- Server/view/account/signup.py
+++ Server/view/account/signup.py
@@ -1,11 +1,33 @@
 from flasgger import swag_from
+from flask_restful import reqparse
 
 from view.base_resource import BaseResource
 from docs.account import SIGNUP_POST
+from model import UserInfo
 
 
 class Signup(BaseResource):
 
     @swag_from(SIGNUP_POST)
     def post(self):
-        pass
+        parser = reqparse.RequestParser
+        parser.add_argument('email', type=str, required=True)
+        parser.add_argument('id', type=str, required=True)
+        parser.add_argument('password', type=str, required=True)
+        email_ = parser.parse_args()['email']
+        id_ = parser.parse_args()['id']
+        password_ = parser.parse_args()['password']
+        # 이메일, ID, 비밀번호 파싱
+
+        db_201 = {
+            "userId": id_,
+            "email": email_,
+            "password": password_
+        }
+
+        if UserInfo.objects(userId__ne=id_) or UserInfo.object(email__ne=email_):
+            UserInfo(userId=id_, email=email_, password=password_, team=1)
+            # DB에 중복되는 닉네임이 없음을 확인하고 신규 회원 정보를 DB에 저장
+            return 201
+        else:
+            return 205
