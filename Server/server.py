66b49570087ef98e6f674ebc2b6f5f9c4fe1d595
RISMME <python@istruly.sexy>
2018-10-18 15:48:40 +0900

    MOVE: hook api to Router class

--- Server/server.py
+++ Server/server.py
@@ -1,27 +1,7 @@
-import os
-import threading
-import time
-
-from flask import request
-
 from app import create_app
 
+
 app = create_app()
-
-
-def reload_server():
-    time.sleep(2)
-
-    os.system('. ../hook.sh')
-
-
-@app.route('/hook', methods=['POST'])
-def webhook_event_handler():
-    if request.headers['X-GitHub-Event'] == 'push':
-        threading.Thread(target=reload_server).start()
-
-    return 'hello'
-
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=1234)
--- Server/view/__init__.py
+++ Server/view/__init__.py
@@ -1,12 +1,30 @@
+import os
+import threading
+import time
+
+from flask import request, Flask
 from flask_restful import Api
 
 
 class Router:
-    def __init__(self, app):
+    def __init__(self, app: Flask):
         self.app = app
         self.api = Api(app)
 
+    def reload_server(self):
+        time.sleep(2)
+
+        os.system('. ../hook.sh')
+
+    def webhook_event_handler(self):
+        if request.headers['X-GitHub-Event'] == 'push':
+            threading.Thread(target=self.reload_server).start()
+
+        return 'hello'
+
     def register(self):
+        self.app.add_url_rule('/hook', view_func=self.webhook_event_handler, methods=['POST'])
+
         from view.account.auth import Auth
         from view.account.signup import Signup
         self.api.add_resource(Auth, '/auth')
