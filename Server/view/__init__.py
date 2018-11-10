6dc84f6f492fc078db34d39e1778d60cb0e751bf
RISMME <python@istruly.sexy>
2018-11-10 20:09:13 +0900

    UPDATE: routing

--- Server/view/__init__.py
+++ Server/view/__init__.py
@@ -5,9 +5,6 @@ import time
 
 from flask import request, Flask, current_app, abort, jsonify
 from flask_restful import Api
-from werkzeug.exceptions import HTTPException
-
-from model.team import TeamModel
 
 
 class Util:
@@ -30,26 +27,6 @@ class Util:
             if current_app.config['END_TIME'] < datetime.now():
                 abort(412)
 
-    def init_team(self):
-        for i in ['empty', 'blue', 'green', 'yellow', 'violet']:
-            team: TeamModel = TeamModel.objects(teamName=i).first()
-            if not team:
-                TeamModel(teamName=i).save()
-
-    def exception_handler(self, e):
-        print(e)
-
-        if isinstance(e, HTTPException):
-            description = e.description
-            code = e.code
-        else:
-            description = ''
-            code = 500
-
-        return jsonify({
-            'msg': description
-        }), code
-
 
 class Router(Util):
     def __init__(self, app: Flask):
@@ -60,27 +37,15 @@ class Router(Util):
         self.app.add_url_rule('/hook', view_func=self.webhook_event_handler, methods=['POST'])
         self.app.before_request(self.time_check)
         self.app.before_first_request(self.init_team)
-        self.app.register_error_handler(Exception, self.exception_handler)
 
-        from view.auth import Auth
-        from view.account.signup import Signup
-        self.api.add_resource(Auth, '/auth')
-        self.api.add_resource(Signup, '/signup')
+        from view.auth import AuthView
+        self.api.add_resource(AuthView, '/auth/<gameKey>')
 
-        from view.map import AndroidMap, WebMap
-        from view.map.rank import Rank
-        self.api.add_resource(AndroidMap, '/map/android')
-        self.api.add_resource(WebMap, '/map/web')
-        self.api.add_resource(Rank, '/rank')
+        from view.map import MapView
+        self.api.add_resource(MapView, '/map')
 
-        from view.solve import Solve
-        self.api.add_resource(Solve, '/solve/<boothName>')
+        from view.solve import SolveView
+        self.api.add_resource(SolveView, '/solve/<boothName>')
 
-        from view.team import Team
-        self.api.add_resource(Team, '/team')
-
-        from view.booth import Booth
-        self.api.add_resource(Booth, '/booth')
-
-        from view.init import InitGame
-        self.api.add_resource(InitGame, '/init')
+        from view.team import TeamView
+        self.api.add_resource(TeamView, '/team')
