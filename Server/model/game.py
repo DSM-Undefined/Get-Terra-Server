6ad5e76f4cf11b5fff0eeb2b72686d47d2be41b0
RISMME <python@istruly.sexy>
2018-11-10 20:11:17 +0900

    UPDATE: add start, end time and team count in game model

--- Server/model/game.py
+++ Server/model/game.py
@@ -5,3 +5,17 @@ class GameModel(Document):
     meta = {
         'collection': 'game'
     }
+
+    start_time = DateTimeField(
+        required=True
+    )
+
+    end_time = DateTimeField(
+        required=True
+    )
+
+    teamCount = IntField(
+        required=True,
+        min_value=1,
+        max_value=100
+    )
