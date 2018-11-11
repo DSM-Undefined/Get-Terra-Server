026dc824f7b90a64dc88f08ae8c9ec26f08a7d72
RISMME <python@istruly.sexy>
2018-11-11 20:13:18 +0900

    UPDATE: auth

--- Server/docs/account.py
+++ Server/docs/account.py
@@ -1,34 +1,5 @@
 from . import parameter
 
-AUTH_POST = {
-    'tags': ['Account'],
-    'description': '로그인',
-    'parameters': [
-        parameter('gameKey', '게임 인증키', 'uri', 'int'),
-        parameter('id', '아이디'),
-        parameter('password', '비밀번호')
-    ],
-    'responses': {
-        '201': {
-            'description': '인증 성공',
-            'examples': {
-                '': {
-                    'accessToken': '액세스 토오오오큰'
-                }
-            }
-        },
-        '204': {
-            'description': '잘못된 게임 인증키'
-        },
-        '205': {
-            'description': '존재하는 아이디'
-        },
-        '401': {
-            'description': '로그인 실패'
-        }
-    }
-}
-
 CHECK_GAME_KEY_GET = {
     'tags': ['Account'],
     'description': '게임키 유효성 확인',
@@ -46,6 +17,32 @@ CHECK_GAME_KEY_GET = {
         },
         '204': {
             'description': '잘못된 게임 인증키'
+        }
+    }
+}
+
+AUTH_POST = {
+    'tags': ['Account'],
+    'description': '로그인',
+    'parameters': [
+        parameter('gameKey', '게임 인증키', 'uri', 'int'),
+        parameter('id', '아이디'),
+        parameter('password', '비밀번호')
+    ],
+    'responses': {
+        '201': {
+            'description': '인증 또는 회원가입 성공',
+            'examples': {
+                '': {
+                    'accessToken': '액세스 토오오오큰'
+                }
+            }
+        },
+        '204': {
+            'description': '잘못된 게임 인증키'
+        },
+        '205': {
+            'description': '잘못된 비밀번호'
         }
     }
 }
--- Server/view/auth.py
+++ Server/view/auth.py
@@ -4,8 +4,6 @@ from flask import request, jsonify, abort
 from flask_jwt_extended import create_access_token
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from datetime import timedelta
-
 from docs.account import AUTH_POST, CHECK_GAME_KEY_GET
 from model.user import UserModel
 from model.game import GameModel
@@ -14,24 +12,24 @@ from util import check_gameKey
 
 class AuthView(Resource):
 
-    def encrypt_password(self, pw):
-        return generate_password_hash(pw)
-
     @check_gameKey
     @swag_from(CHECK_GAME_KEY_GET)
     def get(self, gameKey):
         if not GameModel.objects(gameKey=gameKey).first():
             abort(204)
-
         return jsonify({'gameKey': gameKey})
 
     @check_gameKey
     @swag_from(AUTH_POST)
     def post(self, gameKey):
         payload = request.json
+        user: UserModel = UserModel.objects(userId=payload['id'], gameKey=gameKey).first()
 
-        user: UserModel = UserModel.objects(userId=payload['id']).first()
-        if check_password_hash(user.password, payload['password']):
-            return jsonify({"accessTocken": create_access_token(identity=user.userId, expires_delta=timedelta(days=1))})
+        if user:
+            if check_password_hash(user.password, payload['password']):
+                return create_access_token(user.id)
+            else:
+                abort(205)
 
-        abort(401)
+        UserModel(payload['id'], generate_password_hash(payload['password']), gameKey).save()
+        return jsonify({'accessToken': create_access_token(user.id)})
